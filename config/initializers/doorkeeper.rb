0ae26bac94dd2bfccb9b3cffda8a7665e1ff5dc2
Neson <neson@dex.tw>
2015-01-19 09:00:34 +0800

    fix bugs on rails-erd

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -99,5 +99,32 @@ Doorkeeper.configure do
   wildcard_redirect_uri true
 end
 
-require Rails.root.join('app', 'models', 'oauth_application')
+# require Rails.root.join('app', 'models', 'concerns', 'oauth_application') unless defined? OAuthApplication
+module OAuthApplication
+  extend ActiveSupport::Concern
+
+  included do
+    scope :user_apps, -> { where("owner_type = ?", 'User') }
+    scope :core_apps, -> { where("owner_type = ?", 'Admin') }
+
+    before_create :set_initial_refresh_time
+  end
+
+  module ClassMethods
+  end
+
+  def core_app?
+    owner_type == 'Admin'
+  end
+
+  private
+
+  def set_initial_refresh_time
+    self.rth_refreshed_at = DateTime.now.change(min: 0, sec: 0)
+    self.rtd_refreshed_at = Date.today
+    self.core_rth_refreshed_at = DateTime.now.change(min: 0, sec: 0)
+    self.core_rtd_refreshed_at = Date.today
+  end
+end
+
 Doorkeeper::Application.send :include, OAuthApplication
