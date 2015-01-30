49b918df159f8c1d06d3da36e5b62d73448450b5
Neson <neson@dex.tw>
2015-01-30 18:27:35 +0800

    attach the user's update time to identity_token, allowing other apps to check and refresh user's information from core

--- app/services/site_identity_token.rb
+++ app/services/site_identity_token.rb
@@ -2,25 +2,26 @@ module SiteIdentityToken
   module MaintainService
     class << self
       def create_cookie_token(cookies, user = current_user)
-        identity_token_cookie = { value: generate_token(user),
-                                  domain: '.' + domain,
-                                  expires: 1.year.from_now }
-        identity_token_cookie.except!(:domain) if Rails.env.test?
-        cookies[:_identity_token] = identity_token_cookie
+        set_identity_token_cookie(cookies, generate_token(user))
       end
 
       def destroy_cookie_token(cookies)
-        identity_token_cookie = { value: '',
-                                  domain: '.' + domain,
-                                  expires: 1.year.from_now }
-        identity_token_cookie.except!(:domain) if Rails.env.test?
-        cookies[:_identity_token] = identity_token_cookie
+        set_identity_token_cookie(cookies, '')
       end
 
       def generate_token(user)
         timestamp = Time.now.to_time.to_i
         token_hash = Digest::MD5.hexdigest(user.id.to_s + Digest::MD5.hexdigest(secret_key + timestamp.to_s))
-        "#{token_hash}.#{timestamp}"
+        user_updated_at = user.updated_at.to_time.to_i
+        "#{token_hash}.#{timestamp}.#{user_updated_at}"
+      end
+
+      def set_identity_token_cookie(cookies, value)
+        identity_token_cookie = { value: value,
+                                  domain: '.' + domain,
+                                  expires: 1.year.from_now }
+        identity_token_cookie.except!(:domain) if Rails.env.test?
+        cookies[:_identity_token] = identity_token_cookie
       end
 
       def secret_key
