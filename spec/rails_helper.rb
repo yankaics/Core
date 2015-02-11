e6a711d5cedd145d90c95b27b93a31d2fff5b852
Neson <neson@dex.tw>
2015-02-11 10:22:17 +0800

    add codeclimate-test-reporter
    #setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ require 'email_spec'
 require 'webmock/rspec'
 require 'capybara-screenshot/rspec'
 Capybara.javascript_driver = :webkit
-WebMock.disable_net_connect!(:allow_localhost => true)
+WebMock.disable_net_connect!(:allow_localhost => true, :allow => ['codeclimate.com'])
 OmniAuth.config.test_mode = true
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
