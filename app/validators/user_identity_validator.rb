182bf29614c26414077fc745ccc49443c6527974
Neson <neson@dex.tw>
2015-02-24 22:19:01 +0800

    fix user email verification error

--- app/validators/user_identity_validator.rb
+++ app/validators/user_identity_validator.rb
@@ -1,10 +1,10 @@
 class UserIdentityValidator < ActiveModel::Validator
   def validate(record)
     # record.department = record.organization.departments.find_by(code: record.department_code) if record.department_code_changed?
-    if record.department.presence || record.department_code.presence
+    if record.department.present?# || record.department_code.present?
       record.errors[:base] << "Illegal department (department is in a different organization?)." if record.organization_code != record.department.organization_code
       return if record.permit_changing_department_in_organization
-      return unless record.original_department_code.presence
+      return unless record.original_department_code.present?
       if record.permit_changing_department_in_group
         record.errors[:base] << "Changing the department to a different group from the original one is not permitted for this identity." if record.department.group != record.original_department.group
       elsif record.department_code != record.original_department_code
