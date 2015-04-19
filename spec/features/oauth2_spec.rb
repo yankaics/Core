2ad33fb4d13f85505711b1441a66f9ff5decb902
Neson <neson@dex.tw>
2015-04-20 05:35:41 +0800

    Test: OAuth 2.0 Client Credentials Grant Flow

--- spec/features/oauth2_spec.rb
+++ spec/features/oauth2_spec.rb
@@ -410,5 +410,44 @@ feature "OAuth 2.0", :type => :feature do
       expect(response).not_to have_key 'access_token'
       expect(response).to have_key 'error'
     end
+
+    # RFC 6749 OAuth 2.0 - Client Credentials Grant
+    # https://tools.ietf.org/html/rfc6749#section-4.4
+    scenario "Client Credentials Grant" do
+      # Client Credentials Grant
+      page.driver.post(<<-URL.squish.delete(' ')
+        /oauth/token?
+        grant_type=client_credentials&
+        client_id=#{@app.uid}&
+        client_secret=#{@app.secret}
+        URL
+      )
+
+      response = JSON.parse(page.body)
+      expect(response).to have_key 'access_token'
+      access_token = response['access_token']
+
+      # test the access token
+      visit "/oauth/token/info?access_token=#{access_token}"
+      response = JSON.parse(page.body)
+      expect(response['application']['uid']).to eq(@app.uid)
+
+      # Client Credentials Grant with HTTP Basic Auth
+      page.driver.browser.basic_authorize(@app.uid, @app.secret)
+      page.driver.post(<<-URL.squish.delete(' ')
+        /oauth/token?
+        grant_type=client_credentials
+        URL
+      )
+
+      response = JSON.parse(page.body)
+      expect(response).to have_key 'access_token'
+      access_token = response['access_token']
+
+      # test the access token
+      visit "/oauth/token/info?access_token=#{access_token}"
+      response = JSON.parse(page.body)
+      expect(response['application']['uid']).to eq(@app.uid)
+    end
   end
 end
