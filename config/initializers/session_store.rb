8171ac7231146ed9840d1e2c4ebd5ce2f5bcf1d9
Neson <neson@dex.tw>
2015-05-03 15:56:59 +0800

    let top-level domain (TLD) features can be disabled by environment variable

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_Core_session', domain: :all
+if ENV['URL_TLD'] == 'false'
+  Rails.application.config.session_store :cookie_store, key: '_Core_session'
+else
+  Rails.application.config.session_store :cookie_store, key: '_Core_session', domain: :all
+end
