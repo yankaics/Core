ae80b860208075ce0aa50bcb0e81883d4a8565e4
Neson <neson@dex.tw>
2015-05-22 20:38:00 +0800

    update Facebook login logic

--- app/models/concerns/omniauth_callable.rb
+++ app/models/concerns/omniauth_callable.rb
@@ -34,14 +34,16 @@ module OmniauthCallable
 
       return false if auth[:info][:email].blank? || auth[:info][:name].blank?
 
-      user = where(fbemail: auth[:info][:email]).first || where(email: auth[:info][:email]).first_or_create! do |new_user|
-        new_user.fbemail = auth[:info][:email]
-        new_user.fbid = auth[:uid]
-        new_user.password = Devise.friendly_token[0, 20]
-        new_user.name = auth[:info][:name]
-        info_name = info['name']
-        new_user.name = info_name if info_name
-      end
+      user = (auth[:uid].present? && where(fbid: auth[:uid]).first) ||
+             where(fbemail: auth[:info][:email]).first ||
+             where(email: auth[:info][:email]).first_or_create! do |new_user|
+               new_user.fbemail = auth[:info][:email]
+               new_user.fbid = auth[:uid]
+               new_user.password = Devise.friendly_token[0, 20]
+               new_user.name = auth[:info][:name]
+               info_name = info['name']
+               new_user.name = info_name if info_name
+             end
 
       user.confirm!
 
@@ -63,6 +65,7 @@ module OmniauthCallable
         user.from = 'facebook'
         user.update_attributes(
           fbid: auth[:uid],
+          fbemail: auth[:info][:email],
           fbtoken: auth[:credentials][:token],
           external_avatar_url: info['picture'] && info['picture']['data'] && info['picture']['data']['url'],
           external_cover_photo_url: info['cover'] && info['cover']['source']
