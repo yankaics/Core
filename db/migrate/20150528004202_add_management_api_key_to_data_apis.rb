8e55753e189024cb897144e82cea5226a6be888c
Neson <neson@dex.tw>
2015-05-28 15:42:11 +0800

    fix migration error

--- db/migrate/20150528004202_add_management_api_key_to_data_apis.rb
+++ db/migrate/20150528004202_add_management_api_key_to_data_apis.rb
@@ -3,8 +3,7 @@ class AddManagementAPIKeyToDataApis < ActiveRecord::Migration
     add_column :data_apis, :management_api_key, :string
 
     DataAPI.find_each do |data_api|
-      next if data_api.management_api_key.present?
-      data_api.update_column(:management_api_key, SecureRandom.urlsafe_base64(64).gsub(/[^a-zA-Z0-9]/, '0'))
+      data_api.save!
     end
 
     change_column :data_apis, :management_api_key, :string, null: false
