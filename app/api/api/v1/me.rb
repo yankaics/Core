5f9d7510b967526ae024820fdf7dc7cf79648ead
Neson <neson@dex.tw>
2015-07-07 18:28:14 +0800

    Feature: PATCH me API

--- app/api/api/v1/me.rb
+++ app/api/api/v1/me.rb
@@ -35,6 +35,70 @@ class API::V1::Me < API::V1
       @user = current_user
     end
 
+    desc "Update data of the current user", {
+      http_codes: APIGuard.access_token_error_codes,
+      notes:  <<-NOTE
+        #{APIGuard.access_token_required_note}
+      NOTE
+    }
+    params do
+      User::EDITABLE_ATTRS.each do |attr|
+        optional "user[#{attr}]"
+      end
+    end
+    patch rabl: 'user' do
+      guard!(scopes: ['write'])
+
+      permitted_attrs = []
+      permitted_attrs += User::PUBLIC_ATTRS if scopes.include? :public
+      permitted_attrs += User::EMAIL_ATTRS if scopes.include? :email
+      permitted_attrs += User::ACCOUNT_ATTRS if scopes.include? :account
+      permitted_attrs += User::FB_ATTRS if scopes.include? :facebook
+      permitted_attrs += User::INFO_ATTRS if scopes.include? :info
+      permitted_attrs += User::IDENTITY_ATTRS if scopes.include? :identity
+      permitted_attrs += User::CORE_ATTRS if current_app.present? && current_app.core_app?
+
+      fieldset_for :user, default: true, permitted_fields: permitted_attrs,
+                          defaults_to_permitted_fields: true
+      fieldset_for :user_identity
+      fieldset_for :user_email
+      fieldset_for :organization
+      fieldset_for :department
+
+      inclusion_for :user, default: true
+
+      permitted_attrs &= User::EDITABLE_ATTRS
+
+      @user = current_user
+
+      ac_params = ActionController::Parameters.new(params)
+      user_params = ac_params.require(:user).permit(*permitted_attrs)
+
+      # translate enums
+      if user_params[:gender].present?
+        case user_params[:gender]
+        when 'male'
+          user_params[:gender] = 1
+        when 'female'
+          user_params[:gender] = 2
+        when 'other'
+          user_params[:gender] = 3
+        when 'unspecified'
+          user_params[:gender] = 0
+        else
+          error! "'#{user_params[:gender]}' is not a valid gender. Valid genders are: 'male' ,'female', 'other' or 'unspecified'.", 400
+        end
+      end
+
+      begin
+        @user.update!(user_params)
+      rescue StandardError => e
+        error! e.to_s, 400
+      end
+
+      @user
+    end
+
     desc "Get emails of the current user", {
       http_codes: APIGuard.access_token_error_codes,
       notes:  <<-NOTE
--- spec/requests/api/v1/me_spec.rb
+++ spec/requests/api/v1/me_spec.rb
@@ -8,79 +8,140 @@ describe "Me API" do
     end
   end
 
-  context "requested with a access token contains public scope" do
-    before do
-      @token = create(:oauth_access_token)
-    end
-
-    it "returns the data of current user" do
-      get "/api/v1/me.json?access_token=#{@token.token}"
-
-      expect(response).to be_success
-      json = JSON.parse(response.body)
-      expect(json['name']).to eq @token.resource_owner.name
-      expect(json['id']).not_to be_blank
-      expect(json['uuid']).not_to be_blank
-      expect(json).not_to have_key 'email'
-      expect(json).not_to have_key 'updated_at'
-      expect(json).not_to have_key 'fbid'
-      expect(json).not_to have_key 'brief'
-      expect(json).not_to have_key 'organization'
-    end
-  end
-
-  {
-    'email' => %w(email),
-    'account' => %w(sign_in_count last_sign_in_at),
-    'facebook' => %w(fbid),
-    'info' => %w(birth_month motto),
-    'identity' => %w(emails identities organizations department uid identity)
-  }.each do |scope, attrs|
-
-    context "requested with a access token contains #{scope} scope" do
+  context "GET" do
+    context "requested with a access token contains public scope" do
       before do
-        @token = create(:oauth_access_token, scopes: "public #{scope}")
+        @token = create(:oauth_access_token)
       end
 
-      attrs.each do |attr|
-        it "returns the #{attr} of current user" do
-          get "/api/v1/me.json?access_token=#{@token.token}"
+      it "returns the data of current user" do
+        get "/api/v1/me.json?access_token=#{@token.token}"
 
-          expect(response).to be_success
-          json = JSON.parse(response.body)
-          expect(json).to have_key attr
+        expect(response).to be_success
+        json = JSON.parse(response.body)
+        expect(json['name']).to eq @token.resource_owner.name
+        expect(json['id']).not_to be_blank
+        expect(json['uuid']).not_to be_blank
+        expect(json).not_to have_key 'email'
+        expect(json).not_to have_key 'updated_at'
+        expect(json).not_to have_key 'fbid'
+        expect(json).not_to have_key 'brief'
+        expect(json).not_to have_key 'organization'
+      end
+    end
+
+    {
+      'email' => %w(email),
+      'account' => %w(sign_in_count last_sign_in_at),
+      'facebook' => %w(fbid),
+      'info' => %w(birth_month motto),
+      'identity' => %w(emails identities organizations department uid identity)
+    }.each do |scope, attrs|
+
+      context "requested with a access token contains #{scope} scope" do
+        before do
+          @token = create(:oauth_access_token, scopes: "public #{scope}")
+        end
+
+        attrs.each do |attr|
+          it "returns the #{attr} of current user" do
+            get "/api/v1/me.json?access_token=#{@token.token}"
+
+            expect(response).to be_success
+            json = JSON.parse(response.body)
+            expect(json).to have_key attr
+          end
         end
       end
     end
+
+    context "requested with a access token having core powers" do
+      before do
+        @token = create(:oauth_access_token, :core, resource_owner_id: create(:user, :with_identity).id)
+      end
+
+      it "includes the user's emails, primary_identity and identities by default" do
+        get "/api/v1/me.json?access_token=#{@token.token}"
+        expect(response).to be_success
+        json = JSON.parse(response.body)
+        expect(json['emails'].first).to be_a(Hash)
+        expect(json['primary_identity']).to be_a(Hash)
+        expect(json['identities'].first).to be_a(Hash)
+        expect(json['_meta']['relations']['emails']).to be_blank
+        expect(json['_meta']['relations']['primary_identity']).to be_blank
+        expect(json['_meta']['relations']['identities']).to be_blank
+      end
+
+      it "inclusion can be set to none" do
+        get "/api/v1/me.json?access_token=#{@token.token}&include=none"
+        expect(response).to be_success
+        json = JSON.parse(response.body)
+        expect(json['emails'].first).to be_a(Integer)
+        expect(json['primary_identity']).to be_a(Integer)
+        expect(json['identities'].first).to be_a(Integer)
+        expect(json['_meta']['relations']['emails']['type']).to eq('user_email')
+        expect(json['_meta']['relations']['primary_identity']['type']).to eq('user_identity')
+        expect(json['_meta']['relations']['identities']['type']).to eq('user_identity')
+      end
+    end
   end
 
-  context "requested with a access token having core powers" do
-    before do
-      @token = create(:oauth_access_token, :core, resource_owner_id: create(:user, :with_identity).id)
+  context "PATCH" do
+    context "requested with a access token contains public scope" do
+      before do
+        @token = create(:oauth_access_token)
+      end
+
+      it "fails" do
+        patch "/api/v1/me.json?access_token=#{@token.token}&user[username]=wahaha&user[gender]=male&user[birth_year]=1994&user[birth_month]=1&&user[birth_day]=1"
+
+        expect(response).not_to be_success
+      end
     end
 
-    it "includes the user's emails, primary_identity and identities by default" do
-      get "/api/v1/me.json?access_token=#{@token.token}"
-      expect(response).to be_success
-      json = JSON.parse(response.body)
-      expect(json['emails'].first).to be_a(Hash)
-      expect(json['primary_identity']).to be_a(Hash)
-      expect(json['identities'].first).to be_a(Hash)
-      expect(json['_meta']['relations']['emails']).to be_blank
-      expect(json['_meta']['relations']['primary_identity']).to be_blank
-      expect(json['_meta']['relations']['identities']).to be_blank
+    context "requested with a access token contains write scope" do
+      before do
+        @token = create(:oauth_access_token, scopes: 'public write')
+      end
+
+      it "updates the user's data" do
+        patch "/api/v1/me.json?access_token=#{@token.token}&user[username]=wahaha&user[gender]=male&user[birth_year]=1994&user[birth_month]=1&&user[birth_day]=1"
+
+        expect(response).to be_success
+        json = JSON.parse(response.body)
+        expect(json['username']).to eq('wahaha')
+        user = User.find(json['id'])
+        expect(user.username).to eq('wahaha')
+        expect(user.gender).to eq('unspecified')
+        expect(user.birth_year).not_to eq(1994)
+      end
     end
 
-    it "inclusion can be set to none" do
-      get "/api/v1/me.json?access_token=#{@token.token}&include=none"
-      expect(response).to be_success
-      json = JSON.parse(response.body)
-      expect(json['emails'].first).to be_a(Integer)
-      expect(json['primary_identity']).to be_a(Integer)
-      expect(json['identities'].first).to be_a(Integer)
-      expect(json['_meta']['relations']['emails']['type']).to eq('user_email')
-      expect(json['_meta']['relations']['primary_identity']['type']).to eq('user_identity')
-      expect(json['_meta']['relations']['identities']['type']).to eq('user_identity')
+    context "requested with a access token with public, info and write scope" do
+      before do
+        @token = create(:oauth_access_token, scopes: 'public info write')
+      end
+
+      it "updates the user's data" do
+        patch "/api/v1/me.json?access_token=#{@token.token}&user[username]=wahaha&user[gender]=male&user[birth_year]=1994&user[birth_month]=1&&user[birth_day]=1"
+
+        expect(response).to be_success
+        json = JSON.parse(response.body)
+        expect(json['username']).to eq('wahaha')
+        user = User.find(json['id'])
+        expect(user.username).to eq('wahaha')
+        expect(user.gender).to eq('male')
+        expect(user.birth_year).to eq(1994)
+        expect(user.birth_month).to eq(1)
+        expect(user.birth_day).to eq(1)
+      end
+
+      it "fails if parameters are invalid" do
+        patch "/api/v1/me.json?access_token=#{@token.token}&user[birth_day]=100"
+        expect(response).not_to be_success
+        patch "/api/v1/me.json?access_token=#{@token.token}&user[gender]=ahg"
+        expect(response).not_to be_success
+      end
     end
   end
 end
