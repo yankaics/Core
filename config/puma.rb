a1fe01dbb1140a39f2807c8e45ad466e4fff41bf
ChengHsuanLiu <chenghsuango@gmail.com>
2016-04-06 02:59:25 +0800

    update daemonize true

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@
 
 environment ENV['RAILS_ENV'] || 'production'
 
-daemonize false
+daemonize true
 
 pidfile "/var/www/core/shared/tmp/pids/puma.pid"
 stdout_redirect "/var/www/core/shared/tmp/log/stdout", "/var/www/core/shared/tmp/log/stderr"
