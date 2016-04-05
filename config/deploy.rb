fa17ee08ad3cae60e8dbead499bf16bb2dcebace
ChengHsuanLiu <chenghsuango@gmail.com>
2016-04-06 00:43:35 +0800

    set stage to production

--- config/deploy.rb
+++ config/deploy.rb
@@ -109,19 +109,19 @@ namespace :puma do
   desc "Start the application"
   task :start do
     queue 'echo "-----> Start Puma"'
-    queue "cd #{app_path} && RAILS_ENV="production" && bin/puma.sh start", :pty => false
+    queue "cd #{app_path} && RAILS_ENV='production' && bin/puma.sh start", :pty => false
   end
 
   desc "Stop the application"
   task :stop do
     queue 'echo "-----> Stop Puma"'
-    queue "cd #{app_path} && RAILS_ENV="production" && bin/puma.sh stop"
+    queue "cd #{app_path} && RAILS_ENV='production' && bin/puma.sh stop"
   end
 
   # desc "Restart the application"
   # task :restart do
   #   queue 'echo "-----> Restart Puma"'
-  #   queue "cd #{app_path} && RAILS_ENV="production" && bin/puma.sh restart"
+  #   queue "cd #{app_path} && RAILS_ENV='production' && bin/puma.sh restart"
   # end
 end
 
