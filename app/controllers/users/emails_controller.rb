d0022c7b8fff9d1f837e84b606f95d274a91c0e5
Neson <neson@dex.tw>
2016-04-15 22:38:47 +0800

    place a view for email confirmations

--- app/controllers/users/emails_controller.rb
+++ app/controllers/users/emails_controller.rb
@@ -56,7 +56,185 @@ class Users::EmailsController < ApplicationController
   def confirm
     @email = UserEmail.find_and_confirm(params[:confirmation_token])
 
-    redirect_to :action => :index
+    if @email.present?
+      render html: <<-EOF.strip_heredoc.html_safe
+        <style>
+          body {
+            background: rgba(246, 155, 134, 1);
+            font-family: 'Open Sans', sans-serif;
+            color: #222;
+          }
+
+          a {
+            color: currentColor;
+            text-decoration: none;
+          }
+
+          p {
+            font-size: 13px;
+          }
+
+          a:hover .card-outmore {
+            background: #2C3E50;
+            color: #fff;
+          }
+
+          a:hover .thecard {
+            box-shadow: 0 10px 50px rgba(0,0,0,.6);
+          }
+
+          .thecard {
+            width: 300px;
+            margin: 5% auto;
+            box-shadow: 0 1px 30px rgba(0,0,0,.4);
+            display: block;
+            background-color: #fff;
+            border-radius: 4px;
+            transition: 400ms ease;
+          }
+
+          .card-img {
+            height: 225px;
+          }
+
+          .card-img img {
+            width:100%;
+            border-radius: 4px 4px 0px 0px;
+          }
+
+          .card-caption {
+            position: relative;
+            background: #ffffff;
+            padding: 15px 25px 15px 25px;
+            border-radius: 0px 0px 4px 4px;
+          }
+
+          .card-outmore {
+            padding: 10px 25px 12px 25px;
+            border-radius: 0px 0px 4px 4px;
+            border-top: 1px solid #e0e0e0;
+            background: #efefef;
+            color: #222;
+            display: inline-table;
+            width: 250px;
+            transition: 400ms ease;
+          }
+
+          .card-outmore h5 {
+            float: left;
+          }
+
+          h1 {
+            font-size: 22px;
+          }
+
+          h5 {
+            margin:0;
+          }
+        </style>
+
+        <a href="https://colorgy.io/">
+          <div class="thecard">
+            <div class="card-caption">
+              <h1>Email 驗證已完成</h1>
+              <p>您已完成學校身份認證，可以繼續使用囉！</p>
+            </div>
+            <div class="card-outmore">
+              <h5>到 colorgy.io 首頁 →</h5>
+            </div>
+          </div>
+        </a>
+      EOF
+    else
+      render html: <<-EOF.strip_heredoc.html_safe
+        <style>
+          body {
+            background: rgba(246, 155, 134, 1);
+            font-family: 'Open Sans', sans-serif;
+            color: #222;
+          }
+
+          a {
+            color: currentColor;
+            text-decoration: none;
+          }
+
+          p {
+            font-size: 13px;
+          }
+
+          a:hover .card-outmore {
+            background: #2C3E50;
+            color: #fff;
+          }
+
+          a:hover .thecard {
+            box-shadow: 0 10px 50px rgba(0,0,0,.6);
+          }
+
+          .thecard {
+            width: 300px;
+            margin: 5% auto;
+            box-shadow: 0 1px 30px rgba(0,0,0,.4);
+            display: block;
+            background-color: #fff;
+            border-radius: 4px;
+            transition: 400ms ease;
+          }
+
+          .card-img {
+            height: 225px;
+          }
+
+          .card-img img {
+            width:100%;
+            border-radius: 4px 4px 0px 0px;
+          }
+
+          .card-caption {
+            position: relative;
+            background: #ffffff;
+            padding: 15px 25px 15px 25px;
+            border-radius: 0px 0px 4px 4px;
+          }
+
+          .card-outmore {
+            padding: 10px 25px 12px 25px;
+            border-radius: 0px 0px 4px 4px;
+            border-top: 1px solid #e0e0e0;
+            background: #efefef;
+            color: #222;
+            display: inline-table;
+            width: 250px;
+            transition: 400ms ease;
+          }
+
+          .card-outmore h5 {
+            float: left;
+          }
+
+          h1 {
+            font-size: 22px;
+          }
+
+          h5 {
+            margin:0;
+          }
+        </style>
+
+        <a href="https://colorgy.io/">
+          <div class="thecard">
+            <div class="card-caption">
+              <h1>啊喔，您使用的驗證連結有誤</h1>
+              <p>可能您的驗證連結已經過期，請嘗試重新再申請驗證一次！</p>
+            </div>
+            <div class="card-outmore">
+              <h5>到 colorgy.io 首頁 →</h5>
+            </div>
+          </div>
+        </a>
+      EOF
+    end
   end
 
   def destroy
