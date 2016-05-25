9c600322716e5b0f00138119331d5197bb182185
Yukai Huang <yukai@colorgy.io>
2016-05-25 18:16:14 +0800

    change access token expired period

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -25,7 +25,7 @@ Doorkeeper.configure do
 
   # Access token expiration time (default 2 hours).
   # If you want to disable expiration, set this to nil.
-  access_token_expires_in 2.months
+  access_token_expires_in 2.days
 
   # Reuse access token for the same resource owner within an application (disabled by default)
   # Rationale: https://github.com/doorkeeper-gem/doorkeeper/issues/383
